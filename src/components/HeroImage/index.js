2be78d2757938ddf090fbeaf6ac71ed4bc9aa64c
nhquyen <quyen@gmail.com>
2022-02-09 19:58:51 +0700

    fix 401

---
+++ src/components/HeroImage/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+// Styles
+import {Wrapper, Content, Text} from './HeroImage.styles';
+
+const HeroImage = (props) => {
+    return (
+        <Wrapper image={props.image}>
+            <Content>
+                <Text>
+                    <h1>{props.title}</h1>
+                    <p>{props.text}</p>
+                </Text>
+            </Content>
+        </Wrapper>
+    );
+}
+
+export default HeroImage;
